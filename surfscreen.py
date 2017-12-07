f354e47874775779c32d1e4cbe3f036bec4f230e
antidemos <antidemos@hotmail.com>
2017-12-07 13:51:28 +0000

    Working for daily downloads from Swanny cam.

--- surfscreen.py
+++ surfscreen.py
@@ -1,22 +1,41 @@
-import urllib
-from datetime import datetime
+
+# python 3
+
+import urllib.request
+from datetime import datetime, timedelta
+import sched, time
+
 
 dir = "/home/pi/GrabbySurf/"
 #print('here we go')
 
 def get_time():	# returns time in the format "0000"
-	now=datetime.now()
-	print(now.hour)
+	now = datetime.now() + timedelta(hours = 8) - timedelta(minutes = 5)
 
-	if now.minute-1 < 10:
-		time = str(now.hour) + "0" + str(now.minute-1)
+	if now.hour < 10:
+		hr_str = "0" + str(now.hour)
 	else:
-		time = str(now.hour)+str(now.minute-1)
+		hr_str = str(now.hour)
+
+	if now.minute < 10:
+		min_str = "0" + str(now.minute)
+	else:
+		min_str = str(now.minute)
+
+	time = hr_str + min_str
 	return time
-#
+
+
 print(get_time())
+#def cam_get(str camswan)
 
-#def cam_get(str cam_swan)
-#filename=get_time()
-#urllib.urlretrieve("http://www.transport.wa.gov.au/imarine/coastaldata/coastcam/archivegfx/camswan/"+filename+".jpg", dir+"cam_swan/"+filename+".jpg")
+s = sched.scheduler(time.time, time.sleep)
 
+def get_pics(sc): 
+	print("Doing stuff...")
+	filename = get_time()
+	urllib.request.urlretrieve("http://www.transport.wa.gov.au/imarine/coastaldata/coastcam/archivegfx/camswan/"+filename+".jpg", dir+"camswan/"+filename+".jpg")
+	s.enter(30, 1, get_pics, (sc,))
+
+s.enter(30, 1, get_pics, (s,))
+s.run()
