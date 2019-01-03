1e6caa5b258a33e949e89b71056249a73c974560
antidemos <antidemos@hotmail.com>
2019-01-03 13:47:44 +0000

    Save files to folders organised by date.

--- surfscreen.py
+++ surfscreen.py
@@ -1,36 +1,42 @@
 
-
 # python 3
 
 import urllib.request
 import datetime, pytz
+import os
 from time import sleep
 
 
 #dir = "/home/pi/GrabbySurf/" # for raspberry pi
 dir = "/root/GrabbySurf/"
 url_root = "http://www.transport.wa.gov.au/imarine/coastaldata/coastcam/archivegfx/"
+cams = ["camswan/", "camtrigg1/", "camlancelin/", "camgeraldton/"]
+
 
 def get_datetime():	# returns time in the format "0000"
 	tz = pytz.timezone('Australia/West')
 	now = datetime.datetime.now(tz=tz) - datetime.timedelta(minutes = 5)
 	time = str(now.hour).zfill(2) + str(now.minute).zfill(2)
 	date = str(now.year)[-2:] + str(now.month).zfill(2) + str(now.day).zfill(2)
-	return date, time
+	return {"date":date, "time":time}
 
-def get_pics(): 
-	print(get_datetime()[1]) # print the time
-	getfile = get_datetime()[1] + ".jpg"
-	savefile = get_datetime()[0] + get_datetime()[1] + ".jpg"
-	urllib.request.urlretrieve(url_root + "camswan/" + getfile, dir + "camswan/" + savefile)
-	urllib.request.urlretrieve(url_root + "camtrigg1/" + getfile, dir + "camtrigg1/" + savefile)
-	urllib.request.urlretrieve(url_root + "camlancelin/" +  getfile, dir + "camlancelin/" + savefile)
-	urllib.request.urlretrieve(url_root + "camgeraldton/" + getfile, dir + "camgeraldton/" + savefile)
+
+def get_pics(dt): 
+	print(dt["time"]) # print the time
+	getfile = dt["time"] + ".jpg"
+	savefile = dt["date"] + dt["time"] + ".jpg"
+	for cam in cams:
+		print(dir + cam + dt["date"])
+		if not os.path.exists(dir + cam + dt["date"]):
+			os.mkdir(dir + cam + dt["date"])
+		urllib.request.urlretrieve(url_root + cam + getfile, dir + cam + dt["date"] + "/" +  savefile)
+
 
 while True:
+	dt = get_datetime()
 	try:
-		if int(get_datetime()[1][:2]) > 3 and int(get_datetime()[1][:2]) < 21:  # get between 4am and 9pm
-			get_pics()
+		if int(dt["time"][:2]) > 3 and int(dt["time"][:2]) < 22:  # get between 4am and 9pm
+			get_pics(dt)
 			sleep(55)
 		else:
 			print("Sleeping") 
@@ -39,6 +45,5 @@ while True:
 		print("URLError")
 	except (OSError):
 		print("OSError")
-		pass
 
 # filecmp to compare files, delete if identical and get missing image
